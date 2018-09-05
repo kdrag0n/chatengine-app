aaefb1c0744eca3e48d5ecbcead39d59741d40b5
kdrag0n <dragon@khronodragon.com>
2018-09-04 17:46:26 -0700

    Remove unused API models

--- app/src/main/java/com/kdragon/android/chatengine/models/APIRequest.kt
+++
@@ -1,3 +0,0 @@
-package com.kdragon.android.chatengine.models
-
-data class APIRequest(val query: String, val session: String)
--- app/src/main/java/com/kdragon/android/chatengine/models/APIResponse.kt
+++
@@ -1,4 +0,0 @@
-package com.kdragon.android.chatengine.models
-
-data class APIResponse(val success: Boolean, val response: String,
-                       val confidence: Double, val error: String? = null)
