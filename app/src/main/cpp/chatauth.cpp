0033a25acaa1fe2d6b5b1fc69db33f592350a8d5
kdrag0n <dragon@khronodragon.com>
2018-09-04 17:06:00 -0700

    cpp: more obfuscating

--- app/src/main/cpp/chatauth.cpp
+++ app/src/main/cpp/chatauth.cpp
@@ -7,6 +7,7 @@
 
 const char* STR_AUTHORIZATION = "Authorization";
 const char* STR_REFERER = "Referer";
+const std::string &equals = "=";
 
 static const std::string b64decode(const std::string &base64) {
     std::string output;
@@ -16,8 +17,8 @@ static const std::string b64decode(const std::string &base64) {
 
 static const char* getReferer() {
     std::string protocol = "https";
-    std::string domain = b64decode("Y2hhdGVuZ2luZS54eXo=");
-    std::string path = b64decode("Y2hhdA==");
+    std::string domain = b64decode("Y2hhdGVuZ2luZS54eXo" + equals);
+    std::string path = b64decode("Y2hhdA" + equals + equals);
     // TODO: base64, IV/decrypt from substratum, obfuscated chars
 
     std::string final = protocol + "://" + domain + "/" + path;
@@ -29,13 +30,18 @@ static const char* getReferer() {
 
 static const char* getKey() {
     std::string base64 = "OTM0ODY2MTUzZDliN2E0Mzg0Mjg2Mzc1MTIyOTg5Y2I3OGMyZWYyOTkyY2RlMGUyODE1OGM3ZWM";
-    std::string type = "W";
+  //std::string base64 = "OTMyODY2MTUzZDliN2E0Mzg0Mjg2Mzc1MTIyOTg5g2I3OGMyZWYyOTkyY2RlMGUyODE1OGM3ZWM";
+    std::string type = "W";//^                                    ^
     std::string prefix = "abb8";
     std::string suffix = "edd";
 
-    char base64suffix = 92;
-    char twoEight = 28;
-    base64 += base64suffix + twoEight; // 'x'
+    /*base64[3] = '0'; // OTM0O...
+    //                     ^
+
+    base64[41] = 'Y'; // ...OTg5g2...
+    //                          ^*/
+
+    base64 += 'x';
 
     std::string final = type + prefix + b64decode(base64);
     final.append(suffix);
